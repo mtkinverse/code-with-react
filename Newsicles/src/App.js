fdff6df5ec8af8b5f65d283a5640b2a79d8ee273
code.tk <117674892+mtkinverse@users.noreply.github.com>
2023-06-27 14:37:55 +0500

    Top loading bar and infinite scroll added

--- Newsicles/src/App.js
+++ Newsicles/src/App.js
@@ -1,27 +1,32 @@
-import React, { Component } from 'react'
+import React , {useState} from 'react'
 import Navbar from './components/Navbar'
 import News from './components/News'
 import { Routes, Route } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
 
-export class App extends Component {
-
-  render() {
-
+const App = () => {
+  const [progress,setProgress] = useState(0);
+const apiKey = process.env.REACT_APP_API_KEY;
+//process.env.REACT_APP_API_KEY;//Note this has become an environment variable , by declaring it in process.env and also starting the name with REACT_APP this is similar to the process.env.PUBLIC_URL we used note we have to rebuild the server to recreate the enviromemt and to add the included variables in env.local, in environment
+const [country , setCountry] = useState('us');
     return (
       <>
-        <Navbar />
+      <div style={{marginTop:'100px'}}>
+        <Navbar setCountry = {setCountry}  country = {country}/>
+        <LoadingBar height={4} color='red' progress={progress}/>
+        </div>
         <Routes>
-          <Route index element = {<News key='general' country = 'us' category='general' MainTitle='Top Headlines : General in USA' pageSize = '20'/>}/>
-          <Route path='/business' element = {<News key='business' country = 'us' category='business' MainTitle='Top Headlines :  Business in USA' pageSize = '20'/>}/>
-          <Route path='/entertainment' element = {<News key='entertainment' country = 'us' category='entertainment' MainTitle='Top Entertainment :  in USA' pageSize = '20'/>}/>
-          <Route path='/health' element = {<News key='health' country = 'us' category='health' MainTitle='Top Headlines : Health in USA' pageSize = '20'/>}/>
-          <Route path='/science' element = {<News key='science' country = 'us' category='science' MainTitle='Top Headlines : Science in USA' pageSize = '20'/>}/>
-          <Route path='/sports' element = {<News key='sports' country = 'us' category='sports' MainTitle='Top Headlines : Sports in USA' pageSize = '20'/>}/>
-          <Route path='/technology' element = {<News key='technology' country = 'us' category='technology' MainTitle='Top Headlines : Technology in USA' pageSize = '20'/>}/>
+          <Route index element = {<News key='general' country = {country} apiKey = {apiKey} category='general' MainTitle='Top Headlines : General in ' pageSize = '20' setProgress={setProgress} />}/>
+          <Route path='/business' element = {<News key='business' country = {country} apiKey = {apiKey} category='business' MainTitle='Top Headlines :  Business in ' pageSize = '20' setProgress={setProgress} />}/>
+          <Route path='/entertainment' element = {<News key='entertainment' country = {country} apiKey = {apiKey} category='entertainment' MainTitle='Top Entertainment :  in ' pageSize = '20' setProgress={setProgress} />}/>
+          <Route path='/health' element = {<News key='health' country = {country} apiKey = {apiKey} category='health' MainTitle='Top Headlines : Health in ' pageSize = '20' setProgress={setProgress} />}/>
+          <Route path='/science' element = {<News key='science' country = {country} apiKey = {apiKey} category='science' MainTitle='Top Headlines : Science in ' pageSize = '20' setProgress={setProgress} />}/>
+          <Route path='/sports' element = {<News key='sports' country = {country} apiKey = {apiKey} category='sports' MainTitle='Top Headlines : Sports in ' pageSize = '20' setProgress={setProgress} />}/>
+          <Route path='/technology' element = {<News key='technology' country = {country} apiKey = {apiKey} category='technology' MainTitle='Top Headlines : Technology in ' pageSize = '20' setProgress={setProgress} />}/>
         </Routes>
       </>
     )
-  }
+  
 }
 
 export default App
