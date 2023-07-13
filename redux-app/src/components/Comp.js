afce32228f3e256ab6966952916087deb1bc5909
Muhammad Taha Khan <mtkinverse@gmaile.com>
2023-07-14 00:08:04 +0500

    A little change in app's component

--- redux-app/src/components/Comp.js
+++ redux-app/src/components/Comp.js
@@ -14,7 +14,8 @@ function Comp() {
             <h3>Update your state here : </h3>
             <button disabled={amount <= 0} className='btn btn-danger mx-2' onClick={() => { dispatch(actionCreators.withdrawMoney(100)) }} >use it</button>
             <button className='btn btn-primary mx-2' onClick={() => { dispatch(actionCreators.depositMoney(100)) }} >Add it</button>
-
+            <br/>
+            <button className='btn btn-dark my-3' onClick={()=>{ dispatch(actionCreators.withdrawMoney(amount))}}>Collect All</button>
         </div>
 
     )
