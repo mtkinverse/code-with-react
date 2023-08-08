a502a3febcd4008741c0f020bfba7f8b4fb24771
Muhammad Taha Khan <mtkinverse@gmaile.com>
2023-08-09 01:22:07 +0500

    Refactoring

--- utility-tool/src/components/AddValues.js
+++ utility-tool/src/components/AddValues.js
@@ -8,27 +8,27 @@ function AddValues() {
     const [Items, setItems] = useState([]);
     const [numbox, setNumbox] = useState(1);
     const [nameHolder, NameEditor] = useState('');
-    const [PrevPosition,setPosition] = useState();
+    const [PrevPosition, setPosition] = useState();
 
 
     const DownloadDiv = (id, name, DivToBeHide) => {
 
         const box = document.getElementById(id);
         const hidingBox = document.getElementById(DivToBeHide);
-        box.setAttribute('style','width:500px;');
-        const prevDisp  = hidingBox.style.display;
-        hidingBox.style.display='none';
+        box.setAttribute('style', 'width:500px;');
+        const prevDisp = hidingBox.style.display;
+        hidingBox.style.display = 'none';
 
         html2Canvas(box).then((canvas) => {
-            const URL = canvas.toDataURL('image/png')
+            const URL = canvas.toDataURL('image/jpg');
             const link = document.createElement('a');
             link.href = URL;
-            link.download = name + '.png';
+            link.download = name ? name + '.jpg' : 'UnnamedBox.jpg';
             link.click();
         })
 
         box.removeAttribute('style');
-        hidingBox.style.display=prevDisp;
+        hidingBox.style.display = prevDisp;
     }
 
     const OpenModal = (index) => {
@@ -60,38 +60,38 @@ function AddValues() {
             <div className='addValues' id='AllValuesHolder'>
                 {
                     Items.map((Item, index) => {
-                        return <div key={Item.id} id={`Cover-${Item.id}`}>
-                            <div className='rightValues' key={Item.id} id={Item.id}>
-                                <span className='badge'>{Item.name ? Item.name : 'Enter name'}</span>
-                                <div className='listHeader'>
-                                    <li >Names</li>
-                                    <li >Values</li>
-                                </div>
+                        return <div key={Item.id} className='covering-box' id={`Cover-${Item.id}`}>
+                                <div className='rightValues' key={Item.id} id={Item.id}>
+                                    <span className='badge'>{Item.name ? Item.name : 'Enter name'}</span>
+                                    <div className='listHeader'>
+                                        <li >Names</li>
+                                        <li >Values</li>
+                                    </div>
 
-                                {
-                                    Item.items.length > 0 ?
-                                        Item.items.map(
-                                            ele => {
-                                                return <ValueComp name={ele.name} value={ele.value} />
-                                            }
-                                        )
-                                        : <div className='listValues'><li><p>Kindly, Enter values</p></li></div>
-                                }
-                                <hr />
-                                <div className='listValues' >
-                                    <li >Total</li>
-                                    <li id='values'>{Item.total}</li>
+                                    {
+                                        Item.items.length > 0 ?
+                                            Item.items.map(
+                                                ele => {
+                                                    return <ValueComp name={ele.name} value={ele.value} />
+                                                }
+                                            )
+                                            : <div className='listValues'><li><p>Kindly, Enter values</p></li></div>
+                                    }
+                                    <hr />
+                                    <div className='listValues' >
+                                        <li >Total</li>
+                                        <li id='values'>{Item.total}</li>
+                                    </div>
+                                    <div className='listValues' id={`Button-holder-${Item.id}`}>
+                                        <li><button id={`Addbtn${numbox}`} className='submitButton' onClick={() => { OpenModal(`modal${index + 1}`) }}>Add Values</button></li>
+                                        <li><button className='submitButton' onClick={e => {
+                                            e.preventDefault();
+                                            NameEditor(Item.name);
+                                            OpenModal(`NameEditor${index + 1}`);
+                                        }}>{Item.name ? 'Rename' : 'Add Name'}</button></li>
+                                        <li><button className='submitButton' onClick={e => { e.preventDefault(); DownloadDiv(`Cover-${Item.id}`, Item.name, `Button-holder-${Item.id}`) }}>Dowload</button></li>
+                                    </div>
                                 </div>
-                                <div className='listValues' id={`Button-holder-${Item.id}`}>
-                                    <li><button id={`Addbtn${numbox}`} className='submitButton' onClick={() => { OpenModal(`modal${index + 1}`) }}>Add Values</button></li>
-                                    <li><button className='submitButton' onClick={e => {
-                                        e.preventDefault();
-                                        NameEditor(Item.name);
-                                        OpenModal(`NameEditor${index + 1}`);
-                                    }}>{Item.name ? 'Rename' : 'Add Name'}</button></li>
-                                    <li><button className='submitButton' onClick={e => { e.preventDefault(); DownloadDiv(`Cover-${Item.id}`, Item.name,`Button-holder-${Item.id}`) }}>Dowload</button></li>
-                                </div>
-                            </div>
                         </div>
                     })
                 }
